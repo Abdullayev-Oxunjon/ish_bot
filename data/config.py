a49331b06b4d2ea94059d0e38e090b26ad73591f
oxunjon <abdullayevoxunjon757@gmail.com>
2023-07-08 15:33:48 +0500

    fayllar

--- data/config.py
+++ data/config.py
@@ -1,7 +1,3 @@
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
 
 BOT_TOKEN="6179230952:AAHr1lhO9lI8VAs7NzDLoq4IvN613JcC85I"
 ADMINS=[6122889250,]
